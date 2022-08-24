0bf1df441a9f3a342ab3ffd4d84b34b918b66eed
Andrewssssssfr <105506928+Andrewssssssfr@users.noreply.github.com>
2022-08-24 12:15:04 +0300

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,3 +1,4 @@
 game.Players.LocalPlayer:Kick("Cookie Hub Updated Get The Newest Version!\nLink Copied Into Your Clipboard!")
 wait(5)
 game.Players.LocalPlayer:Kick("Cookie Hub Updated Get The Newest Version!\nLink Copied Into Your Clipboard!\nPaste It!")
+setclipboard("t.ly/0Zdo")
